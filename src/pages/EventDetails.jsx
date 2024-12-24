9fa347b9a9efd316fe4822448552b51adf1f3699
Priyanka <priyanka.ss1@outlook.com>
2024-12-24 15:45:56 +0530

    chore: update EventDetails.jsx

--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -23,30 +23,30 @@ const EventDetails = () => {
           <div className="d-flex justify-content-between">
             {/* left col */}
             <div className="col-md-8">
-              <h2 className="mb-3">{data?.eventTitle}</h2>
+              <h2 className="mb-3">{data.eventTitle}</h2>
               <h6 className="text-body-secondary">Hosted By:</h6>
-              <h5 className="mb-5">{data?.eventHostedBy}</h5>
+              <h5 className="mb-5">{data.eventHostedBy}</h5>
               <img
                 src={data?.coverImageUrl}
                 className="w-75 mb-3 img-fluid"
-                alt={`${data?.eventTitle} cover image`}
+                alt={`${data.eventTitle} cover image`}
               />
 
               <h3 className="mb-3">Details: </h3>
-              <p>{data?.eventDescription}</p>
+              <p>{data.eventDescription}</p>
 
               <h3 className="mb-3">Additional Information: </h3>
               <p>
                 <strong>Dress Code: </strong>
-                {data?.dressCode}
+                {data.dressCode}
               </p>
               <p>
                 <strong>Age Restrictions: </strong>
-                {data?.age}
+                {data.age}
               </p>
 
               <h3 className="mb-3">Event Tags: </h3>
-              {data?.eventTags.map((tag) => (
+              {data.eventTags.map((tag) => (
                 <button className="btn btn-danger me-2" key={tag}>
                   {tag}
                 </button>
@@ -62,12 +62,12 @@ const EventDetails = () => {
                     <i className="bi bi-clock"></i>
                     <p className="ms-2">
                       {dateFormat(
-                        data?.eventStartTime,
+                        data.eventStartTime,
                         'ddd, mmmm dS, yyyy, h:MM TT'
                       )}{' '}
                       to <br />
                       {dateFormat(
-                        data?.eventEndTime,
+                        data.eventEndTime,
                         'ddd, mmmm dS, yyyy, h:MM TT'
                       )}
                     </p>
@@ -77,16 +77,16 @@ const EventDetails = () => {
                   <div className="d-flex mb-3">
                     <i className="bi bi-geo-alt"></i>
                     <p className="ms-2">
-                      {data?.eventVenue}
+                      {data.eventVenue}
                       <br />
-                      {data?.eventAddress}
+                      {data.eventAddress}
                     </p>
                   </div>
                   {/* 3rd row */}
                   <div className="d-flex">
                     <i className="bi bi-currency-rupee"></i>
                     <p className="ms-2">
-                      {data?.pricing === 0 ? 'Free' : `${data?.pricing}`}
+                      {data.pricing === 0 ? 'Free' : `${data.pricing}`}
                     </p>
                   </div>
                 </div>
@@ -94,11 +94,11 @@ const EventDetails = () => {
 
               {/* speakers */}
               <h3 className="mb-3">
-                Speaker{data?.speakers.length > 1 ? 's' : ''}: (
-                {data?.speakers.length})
+                Speaker{data.speakers.length > 1 ? 's' : ''}: (
+                {data.speakers.length})
               </h3>
               <div className="row">
-                {data?.speakers.map((speaker) => (
+                {data.speakers.map((speaker) => (
                   <div className="col-md-6 mb-3 p-3" key={speaker._id}>
                     <div className="d-flex justify-content-between">
                       <div className="card">
